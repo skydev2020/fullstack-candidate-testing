edf38a3e870b52b72ee9d6bfd6da18ab7c990aaa
skydev2020 <skydev2020@outlook.com>
2020-12-07 01:31:39 -0500

    demo project

--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -1,15 +1,66 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import filters from '../../data/filters';
+import jslinq from 'jslinq';
 
-export default async (req, res) => {
-  res.statusCode = 200;
-  // @todo: implement filters and search
-  // @todo: implement automated tests
+import jobs from '../../data/jobs.json';
 
-  // this timeout emulates unstable network connection, do not remove this one
-  // you need to figure out how to guarantee that client side will render
-  // correct results even if server-side can't finish replies in the right order
-  await new Promise((resolve)=>setTimeout(resolve, 1000 * Math.random()));
+export default async function getJobs(req, res) {
+  let query = {
+    keyword: '',
+    filters: {},
+    sort: {},
+    ...req.body
+  };
   
-  res.json({})
+  res.statusCode = 200;
+
+  let results = [];
+  jobs.map(job => {
+
+    let found = jslinq(job.items).where(el => {
+      let matched = true;
+      let values = jslinq(Object.values(el));
+      if (query.keyword) {
+        matched &= values.where(v => {
+          if (typeof v === 'string') {
+            return v.indexOf(query.keyword) !== -1 || v.toLowerCase().indexOf(query.keyword) !== -1 || v.toUpperCase().indexOf(query.keyword) !== -1;
+          }
+          return false;
+        }).count();
+      }
+
+      Object.keys(query.filters).map( filter => {
+        if ( query.filters[filter].length !== 1 ) {
+          matched &= (jslinq(el[filter]).intersect(query.filters[filter]).count() === query.filters[filter].length);
+        } else {
+          matched &= (el[filter] === query.filters[filter][0]);
+        }
+      })
+
+      return matched;
+
+    });
+
+    const sortFunc = (el, s) => {
+      let coordiate = el[s].split(',');
+      return coordiate[0];
+    }
+
+    Object.keys(query.sort).map(s => {
+      if (query.sort[s] === 'asc') {
+        found = found.orderBy(el => sortFunc(el, s));
+      } else {
+        found = found.orderByDescending(el => sortFunc(el, s));
+      }
+    });
+
+    if (found.count()) {      
+      results.push({
+        ...job,        
+        total_jobs_in_hospital: found.count(),
+        items: found.toList()
+      })
+    }    
+  });
+  
+  return res.json(results);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,42 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setKeyword } from '../store/jobs.action'
+import { Filters, Jobs } from '../components';
+import axios from 'axios'
 
-export default function Home() {
+const Home = ({filters}) => {
+  const dispatch = useDispatch()
+  const state = useSelector((state) => state)  
+  const { query } = state.jobs
+  useEffect(() => {
+    dispatch(setKeyword(''))
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div className="bg-gray-100 p-1">
+      <div className="flex flex-col m-3">
+          <input type="search" className="bg-white shadow border-0 h-10 pt-0.5 pb-0.5 pl-10 pr-3" placeholder="Search for any job, title, keywords or company" value={query.keyword} onChange={(e)=>dispatch(setKeyword(e.target.value))}/>
+          <div className="absolute pl-2 pr-1 w-8 mt-2.5">
+              <img src="https://cdns.iconmonstr.com/wp-content/assets/preview/2018/240/iconmonstr-search-thin.png" />
+          </div>
+      </div>
+      <div className="flex sidebarContainer m-3 lg:space-x-4">
+          <Filters filters={filters} />
+          <Jobs />
+      </div>
     </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  let data = await axios.get(`${process.env.BASE_URL}/api/filters`).then(response => {
+    return response.data;
+  });
+  return {
+    props: { 
+      filters: data    
+    }
+  }
+};
+
+export default Home;
